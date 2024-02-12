118fdaf9fce066990e59dace2207eab6ef255bc4
Loy Hui Chien <132521921+kardios@users.noreply.github.com>
2024-02-12 20:00:38 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,48 +1,39 @@
 import streamlit as st
-import google.generativeai as genai
-from PyPDF2 import PdfReader
 import os
 import time
+from openai import OpenAI
+from langchain_community.document_loaders import UnstructuredFileLoader
 
 # Retrieve the OpenAI API key from the environment variables
-API_KEY = os.environ["GOOGLE_API_KEY"]
-genai.configure(api_key=API_KEY)
-model = genai.GenerativeModel('gemini-pro')
+CLIENT_API_KEY = os.environ['OPENAI_API_KEY']
+client = OpenAI(api_key=CLIENT_API_KEY)
 
-st.write("Hougang Workshop :sunglasses: Google's Gemini Pro Large Language Model")
-
-Menu_Option = st.selectbox("**Select** analysis:", ('Summarise the key points of the text', 'Identify possible biases in the text', 'Seek views disagreeing with the text', 'Find angles missing from the text', 'Discuss broader significance of the topics', 'Compare the text with historical events', 'Customise your own unique prompt'))
-if Menu_Option == "Summarise the key points of the text":
-  instruction = "Summarise the key points of the text."
-elif Menu_Option == "Identify possible biases in the text":
-  instruction = "Identify possible biases in the text."
-elif Menu_Option == "Seek views disagreeing with the text":
-  instruction = "Offer perspectives that disagree with the text."
-elif Menu_Option == "Find angles missing from the text":
-  instruction = "Offer perspectives that are missing from the text."
-elif Menu_Option == "Discuss broader significance of the topics":
-  instruction = "Draft a conclusion that highlights the broader significance of the topics."
-elif Menu_Option == "Compare the text with historical events":
-  instruction = "Reflect on the text and draw similiarities and differences to historical events in the last century."
-elif Menu_Option == "Customise your own unique prompt":
-  instruction = st.text_input("Customise your own unique prompt:", "What are the follow up actions?")
+st.write("Hougang Workshop :sunglasses: Prototyping the next Readhacker")
 
 uploaded_file = st.file_uploader("**Upload** the PDF document to analyse:", type = "pdf")
+
 raw_text = ""
 if uploaded_file is not None:
-  doc_reader = PdfReader(uploaded_file)
-  for i, page in enumerate(doc_reader.pages):
-    text = page.extract_text()
-    if text:
-      raw_text = raw_text + text + "\n"
+  loader = UnstructuredFileLoader(uploaded_file)
+  docs = loader.load()
+  for doc in docs:
+    doc_text = doc.page_content
+    if doc_text:
+      raw_text = raw_text + doc_text + "\n"
 
-if st.button('Let\'s Go!'):
   start = time.time()
-  input = instruction + "\n\n" + raw_text
-  if raw_text.strip() != '': 
-    input = "Read the text below. " + input
-  response = model.generate_content(input)
+  
+  input = "Generate a concise and coherent summary that captures the main ideas and key details:\n\n" + raw_text
+  response = client.chat.completions.create(
+    model = "gpt-3.5-turbo-0125", messages=[
+      {"role": "system", "content": "You are an expert at summarizing documents with accuracy and precision."},
+      {"role": "user", "content": input},
+    ],
+    temperature=0,)
+  )
+  output_text = response.choices[0].message.content
   end = time.time()
-  st.write(response.prompt_feedback)
-  st.write(response.text)
+
+  st.write(output_text)
   st.write("Time to generate: " + str(round(end-start,2)) + " seconds")
+  st.download_button(':scroll:', output_text)
