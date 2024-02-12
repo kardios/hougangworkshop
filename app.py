fcddd46cb011083829e2751270642a608c246968
Loy Hui Chien <132521921+kardios@users.noreply.github.com>
2024-02-12 20:42:16 +0800

    Update app.py

--- app.py
+++ app.py
@@ -11,12 +11,12 @@ client = OpenAI(api_key=CLIENT_API_KEY)
 st.write("Hougang Workshop :sunglasses: Prototyping the next Readhacker from my Garage")
 link = st.text_input("Enter URL:","https://www.channelnewsasia.com")
 
+# Activate URL extraction and LLM inference when button is pressed
 if st.button('Let\'s Go!'):
+  start = time.time()
   loader = UnstructuredURLLoader(urls=[link])
   data = loader.load()
   raw_text = data[0].page_content
-
-  start = time.time()
   input = "Generate a concise and coherent summary that captures the main ideas and key details:\n\n" + raw_text
   response = client.chat.completions.create(
     model = "gpt-3.5-turbo-0125", messages=[
@@ -25,10 +25,8 @@ if st.button('Let\'s Go!'):
     ],
     temperature=0,
   )
-  
   output_text = response.choices[0].message.content
   end = time.time()
-
   st.write(output_text)
   st.write("Time to generate: " + str(round(end-start,2)) + " seconds")
   st.download_button(':scroll:', output_text)
