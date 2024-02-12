eab4f8b6cbbec019df97695e530b799251a57f73
Loy Hui Chien <132521921+kardios@users.noreply.github.com>
2024-02-12 20:40:12 +0800

    Update app.py

--- app.py
+++ app.py
@@ -9,22 +9,14 @@ CLIENT_API_KEY = os.environ['OPENAI_API_KEY']
 client = OpenAI(api_key=CLIENT_API_KEY)
 
 st.write("Hougang Workshop :sunglasses: Prototyping the next Readhacker from my Garage")
+link = st.text_input("Enter URL:","https://www.channelnewsasia.com")
 
-uploaded_file = st.file_uploader("**Upload** the PDF document to analyse:", type = "pdf")
-
-raw_text = ""
-if uploaded_file is not None:
-  st.write("UPLOADED")
-  st.write(uploaded_file)
-  loader = UnstructuredFileLoader(uploaded_file)
-  docs = loader.load()
-  for doc in docs:
-    doc_text = doc.page_content
-    if doc_text:
-      raw_text = raw_text + doc_text + "\n"
+if st.button('Let\'s Go!'):
+  loader = UnstructuredURLLoader(urls=[link])
+  data = loader.load()
+  raw_text = data[0].page_content
 
   start = time.time()
-  
   input = "Generate a concise and coherent summary that captures the main ideas and key details:\n\n" + raw_text
   response = client.chat.completions.create(
     model = "gpt-3.5-turbo-0125", messages=[
