c39b22a5e3d30a384c04f5eb6a0eb62d0907caf8
Loy Hui Chien <132521921+kardios@users.noreply.github.com>
2024-02-13 23:49:07 +0800

    Update app.py

--- app.py
+++ app.py
@@ -33,7 +33,7 @@ if uploaded_file is not None:
   output_text = response.choices[0].message.content
   end = time.time()
 
-  st.write(response.choices[0])
+  st.write(response.usage)
   st.write(output_text)
   st.write("Time to generate: " + str(round(end-start,2)) + " seconds")
   st.download_button(':scroll:', output_text)
