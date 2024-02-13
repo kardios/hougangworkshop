587eb2da72d60e0a936df5971e40a5caaadb5839
Loy Hui Chien <132521921+kardios@users.noreply.github.com>
2024-02-14 00:12:51 +0800

    Update app.py

--- app.py
+++ app.py
@@ -3,6 +3,22 @@ import os
 import time
 from openai import OpenAI
 from pypdf import PdfReader
+import pandas as pd
+import numpy as np
+
+df = pd.DataFrame({
+    "col1": np.random.randn(1000) / 50 + 37.76,
+    "col2": np.random.randn(1000) / 50 + -122.4,
+    "col3": np.random.randn(1000) * 100,
+    "col4": np.random.rand(1000, 4).tolist(),
+})
+
+st.map(df,
+    latitude='col1',
+    longitude='col2',
+    size='col3',
+    color='col4')
+
 
 # Retrieve the OpenAI API key from the environment variables
 CLIENT_API_KEY = os.environ['OPENAI_API_KEY']
