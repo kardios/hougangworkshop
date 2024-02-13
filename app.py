704272923fe6be7057acfe6cd62a111853551599
Loy Hui Chien <132521921+kardios@users.noreply.github.com>
2024-02-13 23:39:41 +0800

    Update app.py

--- app.py
+++ app.py
@@ -2,31 +2,37 @@ import streamlit as st
 import os
 import time
 from openai import OpenAI
-from langchain_community.document_loaders import UnstructuredURLLoader
+from pypdf import PdfReader
 
 # Retrieve the OpenAI API key from the environment variables
 CLIENT_API_KEY = os.environ['OPENAI_API_KEY']
 client = OpenAI(api_key=CLIENT_API_KEY)
 
 st.write("Hougang Workshop :sunglasses: Prototyping the next Readhacker from my Garage")
-link = st.text_input("Enter URL:","https://www.channelnewsasia.com")
 
-# Activate URL extraction and LLM inference when button is pressed
-if st.button('Let\'s Go!'):
+uploaded_file = st.file_uploader("**Upload** the PDF document to analyse:", type = "pdf")
+raw_text = ""
+if uploaded_file is not None:
+  doc_reader = PdfReader(uploaded_file)
+  for i, page in enumerate(doc_reader.pages):
+    text = page.extract_text()
+    if text:
+      raw_text = raw_text + text + "\n"
+
+  instruction = "Generate a concise and coherent summary of the text below. Include the main ideas and key details. Present your output in bullet points."
   start = time.time()
-  loader = UnstructuredURLLoader(urls=[link])
-  data = loader.load()
-  raw_text = data[0].page_content
-  input = "Generate a concise and coherent summary that captures the main ideas and key details:\n\n" + raw_text
+  input = instruction + "\n\n" + raw_text
+  
   response = client.chat.completions.create(
-    model = "gpt-3.5-turbo-0125", messages=[
-      {"role": "system", "content": "You are an expert at summarizing documents with accuracy and precision."},
-      {"role": "user", "content": input},
-    ],
-    temperature=0,
-  )
+    model="", messages=[
+        {"role": "system", "content": "You are a diligent and careful intern. You are able to digest articles and essays, and produce quality analysis."},
+        {"role": "user", "content": input},
+      ],
+      temperature=0,
+    )
   output_text = response.choices[0].message.content
   end = time.time()
+  
   st.write(output_text)
   st.write("Time to generate: " + str(round(end-start,2)) + " seconds")
   st.download_button(':scroll:', output_text)
