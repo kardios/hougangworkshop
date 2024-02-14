3060bcdaa8bfbd5ab08c5d39dc080a9db2858367
Loy Hui Chien <132521921+kardios@users.noreply.github.com>
2024-02-14 22:42:40 +0800

    Update app.py

--- app.py
+++ app.py
@@ -29,8 +29,8 @@ with st.expander("Click to expand documentation"):
   st.write("- :red[**Answers may not be suitable or accurate**]")
   st.write("- :blue[**Try reloading webpage to troubleshoot**]")
 
-Option_Input = st.selectbox("Select input:", ('Upload a pdf','Enter free text'))
-Option_Action = st.selectbox("Select action:", ('Condense the text into bullet points', 'Shorten the text into a summary', 'Identify possible biases in the text', 'Seek views disagreeing with the text', 'Find angles missing from the text', 'Discuss broader significance of the topics', 'Compare the text with historical events', 'Customise your own unique prompt'))
+Option_Input = st.selectbox("How will I receive your input?", ('Upload a pdf','Enter free text'))
+Option_Action = st.selectbox("What should I do with the input?", ('Condense the text into bullet points', 'Shorten the text into a summary', 'Identify possible biases in the text', 'Seek views disagreeing with the text', 'Find angles missing from the text', 'Discuss broader significance of the topics', 'Compare the text with historical events', 'Customise your own unique prompt'))
 
 uploaded_file = st.file_uploader("**Upload** a PDF to summarise or analyse:", type = "pdf")
 raw_text = ""
