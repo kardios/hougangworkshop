55c1b8cf65b7ab165da83c64fd76ca73dc2bdf56
Loy Hui Chien <132521921+kardios@users.noreply.github.com>
2024-02-14 22:44:42 +0800

    Update app.py

--- app.py
+++ app.py
@@ -8,7 +8,8 @@ from pypdf import PdfReader
 CLIENT_API_KEY = os.environ['OPENAI_API_KEY']
 client = OpenAI(api_key=CLIENT_API_KEY)
 
-st.write("**Readhacker** :sunglasses: AI-Powered Reading and Ideation")
+st.write("Welcome to **Readhacker**")
+st.write("I am your AI-powered reading and ideation assistant")
 
 # Set the initial temperature, model ID and maximum_tokens
 temperature = 0
@@ -30,7 +31,7 @@ with st.expander("Click to expand documentation"):
   st.write("- :blue[**Try reloading webpage to troubleshoot**]")
 
 Option_Input = st.selectbox("How will I receive your input?", ('Upload a pdf','Enter free text'))
-Option_Action = st.selectbox("What should I do with the input?", ('Condense the text into bullet points', 'Shorten the text into a summary', 'Identify possible biases in the text', 'Seek views disagreeing with the text', 'Find angles missing from the text', 'Discuss broader significance of the topics', 'Compare the text with historical events', 'Customise your own unique prompt'))
+Option_Action = st.selectbox("What should I do with your input?", ('Condense the text into bullet points', 'Shorten the text into a summary', 'Identify possible biases in the text', 'Seek views disagreeing with the text', 'Find angles missing from the text', 'Discuss broader significance of the topics', 'Compare the text with historical events', 'Customise your own unique prompt'))
 
 uploaded_file = st.file_uploader("**Upload** a PDF to summarise or analyse:", type = "pdf")
 raw_text = ""
