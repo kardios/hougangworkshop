418b57213a2e846eedad87531e66b510d59a6d41
Loy Hui Chien <132521921+kardios@users.noreply.github.com>
2024-08-31 17:11:10 +0800

    Update app.py

--- app.py
+++ app.py
@@ -141,7 +141,8 @@ if raw_text.strip() != "":
         headers_perplexity = {"accept": "application/json",
                               "content-type": "application/json",
                               "Authorization": f'Bearer {LLAMA3_API_KEY}'}
-        response_llama = requests.post(get_url_perplexity,   json=payload_perplexity, headers=headers_perplexity)
+        response_llama = requests.post(get_url_perplexity, json=payload_perplexity, headers=headers_perplexity)
+        st.write(response_llama)
         data_llama = json.loads(response_llama.text)
         output_text = data_llama['choices'][0]['message']['content'] 
       
