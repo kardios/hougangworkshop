23260bd4d18b8acb5436a2f49815608a4087a603
Loy Hui Chien <132521921+kardios@users.noreply.github.com>
2024-08-31 11:33:54 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,35 +1,39 @@
 import streamlit as st
 import os
-import telebot
-import io
 import time
+from anthropic import Anthropic
+import google.generativeai as genai
+from google.generativeai.types import HarmCategory, HarmBlockThreshold
 from openai import OpenAI
 from pypdf import PdfReader
 
-recipient_user_id = os.environ['RECIPIENT_USER_ID']
-bot_token = os.environ['BOT_TOKEN']
-bot = telebot.TeleBot(bot_token)
+# Retrieve the API keys from the environment variables
+CLIENT_API_KEY = os.environ['OPENAI_API_KEY']
+CLAUDE_API_KEY = os.environ["ANTHROPIC_API_KEY"]
+GEMINI_API_KEY = os.environ["GOOGLE_API_KEY"]
+client = OpenAI(api_key=CLIENT_API_KEY)
+anthropic = Anthropic(api_key=CLAUDE_API_KEY)
+genai.configure(api_key=GEMINI_API_KEY)
 
-client = OpenAI(api_key=os.environ['OPENAI_API_KEY'])
-st.set_page_config(page_title="Readhacker", page_icon=":sunglasses:",)
-st.write("**Readhacker**, your AI reading and ideation assistant")
+safety_settings = {
+  HarmCategory.HARM_CATEGORY_HARASSMENT: HarmBlockThreshold.BLOCK_NONE,
+  HarmCategory.HARM_CATEGORY_HATE_SPEECH: HarmBlockThreshold.BLOCK_NONE,
+  HarmCategory.HARM_CATEGORY_SEXUALLY_EXPLICIT: HarmBlockThreshold.BLOCK_NONE,
+  HarmCategory.HARM_CATEGORY_DANGEROUS_CONTENT: HarmBlockThreshold.BLOCK_NONE,
+}
 
-file = open('text.txt','r')
-content = file.read()
-file.close()
-st.write(content)
+generation_config = genai.GenerationConfig(candidate_count = 1, temperature = 0)
 
-maximum_tokens = 120000
-model_id = "gpt-4o"
-#model_id = "gpt-4-turbo"
-
-st.write("Uses", model_id)
+st.set_page_config(page_title="Readhacker Beta", page_icon=":sunglasses:",)
+st.write("**Readhacker Beta**, your smart reading and ideation assistant")
 
 with st.expander("Click to read documentation"):
   st.write("- Productivity app by **Sherwood Analytica**")
   st.write("- Upload a PDF or enter free text as input")
   st.write("- Generate a summary or analysis of input") 
-  st.write("- Uses GPT-4-Turbo :robot_face: up to 90,000 words") 
+  st.write("- GPT-4 Omni - up to 128,000 tokens") 
+  st.write("- Claude 3.5 Sonnet - up to 200,000 tokens")
+  st.write("- Gemini 1.5 Pro - up to 2 million tokens") 
   st.write("- :red[**Answers may not be suitable or accurate**]")
   st.write("- :blue[**Try reloading webpage to troubleshoot**]")
 
@@ -59,53 +63,3 @@ elif Option_Action == "Generate markdown summary":
 elif Option_Action == "Customise your own prompt":
   instruction = "You are my reading assistant. You will read the input I provide." + st.text_input("Customise your own unique prompt:", "What are the follow up actions?")
 
-if Option_Input == "Upload a pdf":
-  uploaded_file = st.file_uploader("Upload a PDF to summarise or analyse:", type = "pdf")
-  raw_text = ""
-  if uploaded_file is not None:
-    doc_reader = PdfReader(uploaded_file)
-    for i, page in enumerate(doc_reader.pages):
-      text = page.extract_text()
-      if text:
-        raw_text = raw_text + text + "\n"
-elif Option_Input == "Enter free text":
-  raw_text = ""
-  input_text = st.text_area("Enter the text you would like me to summarize or analyse and click **Let\'s Go :rocket:**")
-  if st.button("Let\'s Go! :rocket:"):
-    raw_text = input_text
-
-if raw_text.strip() != "":
-  try:
-    with st.spinner("Running AI model for text generation..."):
-      start = time.time()
-      input = "Below is the input:\n\n" + raw_text
-      response = client.chat.completions.create(
-        model=model_id, messages=[
-          {"role": "system", "content": instruction},
-          {"role": "user", "content": input},
-        ],
-        temperature=0,
-      )
-      end = time.time()
-    output_text = response.choices[0].message.content
-    container = st.container(border=True)
-    container.write(Option_Action)
-    container.write(output_text)
-    container.write("Time to generate: " + str(round(end-start,2)) + " seconds")
-    container.write(response.usage)
-    st.download_button(':floppy_disk:', output_text)
-    if st.button(':fax:'):
-      bot.send_message(chat_id=recipient_user_id, text=output_text)
-    if st.button(':speech_balloon:'):
-      with st.spinner("Running AI model for audio generation..."):
-        start = time.time()
-        tts_response = client.audio.speech.create(
-          model = "tts-1-hd",
-          voice = "alloy",
-          input = output_text,
-        )
-        end = time.time()
-        st.write("Time to generate: " + str(round(end-start,2)) + " seconds")
-        st.audio(tts_response.content, format="audio/mpeg")
-  except:
-    st.error(" Input length may be too long.", icon="🚨")
